4292a6cbaa87b424d61cc5197e0c51bd8a1479b7
zafar <zafarkudratov@gmail.com>
2019-11-24 08:18:02 +0200

    all

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const enableWs = require('express-ws');
+let fs = require('fs');
+const app = express();
+const expressWs = enableWs(app);
+app.use(express.static('public'));
+
+const port = process.env.PORT || 3000;
+
+
+app.listen(port, function () {
+    console.log('Server is working now, click http://127.0.0.1:3000');
+});
